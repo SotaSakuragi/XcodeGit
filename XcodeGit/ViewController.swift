3751946475495c2bb32f9cd1a36009849b5a6a5e
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-08-27 13:31:40 +0900

    Initial Commit

---
+++ XcodeGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XcodeGit
+//
+//  Created by 櫻木颯大 on 2023/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
